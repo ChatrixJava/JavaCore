a06b9f13bdf7aaab8e7fff2112abe5f9bf14f6b2
ChatrixJava <semchyonok.it@gmail.com>
2021-05-06 16:56:47 +0500

    EffectiveAlgoritm

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,25 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Chatrix First Commit Testing !");
+        System.out.println(fibEffective(100));
     }
+
+    // Число Фино с помощью рекурсии
+    private static long fibNaive(int n) {
+        if (n<=1)
+            return n;
+        return fibNaive(n-1) + fibNaive(n-2);
+    }
+// Число Фино с помощью массива
+    private static long fibEffective(int n) {
+        long [] arr = new long[n+1];
+        arr[0] = 0;
+        arr[1] = 1;
+
+        for (int i = 2; i <= n ; i++)
+            arr[i] = arr[i-1] + arr[i-2];
+
+            return arr[n];
+        }
 }
+
